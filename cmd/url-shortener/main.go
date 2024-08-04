a6e894d103d756b891ba28d81ef33fadbcc889fd
Mystex <valera.kirichenko@gmail.com>
2024-08-04 23:05:00 +0500

    Logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,21 +1,56 @@
 package main
 
 import (
-	"fmt"
-	"url-shortener/internal/config"
+    "fmt"
+    "log/slog"
+    "os"
+    "url-shortener/internal/config"
+)
+
+const (
+    envLocal = "local"
+    envDev   = "dev"
+    envProd  = "prod"
 )
 
 func main() {
-	// TODO: init config: cleanenv
-	cfg := config.MustLoad()
+    // TODO: init config: cleanenv
+    cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+    fmt.Println(cfg)
 
-	// TODO: init logger: slog
+    log := setupLogger(cfg.Env)
+    log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
 
-	// TODO: init storage: sqlite
+    log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+    log.Debug("logger debug mode enabled")
 
-	// TODO: init router: chi, "chi render"
+    // TODO: init logger: slog
 
-	// TODO: run server
+    // TODO: init storage: sqlite
+
+    // TODO: init router: chi, "chi render"
+
+    // TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
+
+    switch env {
+    case envLocal:
+        log = slog.New(
+            slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envDev:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envProd:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+        )
+    }
+
+    return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,44 +1,46 @@
 package config
 
 import (
-    "time"
-    "os"
-    "log"
-    "github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-    Env	            string          `yaml:"env" env-default:"local"`
-    StoragePath     string          `yaml:"storage_path" env-required:"true"`
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-    Address         string          `yaml:"address" env-default:"localhost:8080"`
-    Timeout         time.Duration   `yaml:"timeout" env-default:"4s"`
-    IdleTimeout     time.Duration   `yaml:"idle_timeout" env-default:"60s"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
-    // Must означает, что функция вместо возврата ошибки паникует
-    // Это используется в редких случаях
-    // При инициации конфига это допустимо
+	// Must означает, что функция вместо возврата ошибки паникует
+	// Это используется в редких случаях
+	// При инициации конфига это допустимо
 
-    configPath := os.Getenv("CONFIG_PATH")
+	configPath := os.Getenv("CONFIG_PATH")
 
-    if configPath == "" {
-        log.Fatal("CONFIG_PATH is not set")
-    }
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
 
-    // check if file exists
-    if _, err := os.Stat(configPath); os.IsNotExist(err) {
-        log.Fatalf("Config file does not exists: %s", configPath)
-    }
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exists: %s", configPath)
+	}
 
-    var cfg Config
+	var cfg Config
 
-    if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-        log.Fatalf("Cannot read config: %s", err)
-    }
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Cannot read config: %s", err)
+	}
 
-    return &cfg
+	return &cfg
 }
